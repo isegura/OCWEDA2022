acf059d126a597eb1cfee84cb93026bcf6d8f97f
Isabel Segura-Bedmar <isegura@inf.uc3m.es>
2023-04-20 18:59:19 +0200

    delete mirror

--- TEMA5/hoja2.py
+++ TEMA5/hoja2.py
@@ -1,14 +1,9 @@
 from bst import BinarySearchTree
 from bintree import BinaryNode
-
-"""Problema 1: Implementa una función (iterativa o recursiva) 
-que reciba un árbol binario de búsqueda (de elementos de tipo entero) 
-y devuelva el elemento más pequeño del árbol. ¿Cuál es su complejidad temporal?
-"""
-
+import bintree as bin
 
 def get_smallest_ite(tree: BinarySearchTree) -> int:
-    """returns the smallest element in tree. O(log n)"""
+    """returns the smallest element in input_tree. O(log n)"""
     result = None
     if tree is None:
         print("Tree is None!!!")
@@ -24,7 +19,7 @@ def get_smallest_ite(tree: BinarySearchTree) -> int:
 
 
 def get_greatest_ite(tree: BinarySearchTree) -> int:
-    """returns the greatest element in tree. O(log n)"""
+    """returns the greatest element in input_tree. O(log n)"""
     result = None
     if tree is None:
         print("Tree is None!!!")
@@ -40,7 +35,7 @@ def get_greatest_ite(tree: BinarySearchTree) -> int:
 
 
 def get_smallest_rec(tree: BinarySearchTree) -> int:
-    """returns the smallest element in tree"""
+    """returns the smallest element in input_tree"""
     result = None
     if tree is None:
         print("Tree is None!!!")
@@ -58,27 +53,24 @@ def _get_smallest_rec(node: BinaryNode) -> int:
             return node.elem
     return result
 
-
 def sum_elements(tree: BinarySearchTree) -> int:
     """returns the sum of the elements. O(n)"""
     result = None
     if tree is None:
-        print("tree is None!!!")
+        print("input_tree is None!!!")
     else:
         elements = tree.inorder_list()
         result = sum(elements)
     return result
 
-
 def sum_elements_rec(tree: BinarySearchTree) -> int:
     """returns the sum of the elements. O(n)"""
     result = None
     if tree is None:
-        print("tree is None!!!")
+        print("input_tree is None!!!")
     else:
         result = _sum_elements_rec(tree.root)
     return result
-
 
 def _sum_elements_rec(node: BinaryNode) -> int:
     """returns the sum of all elements in the subtree node"""
@@ -125,7 +117,74 @@ def _remove(node: BinaryNode, elem: object) -> BinaryNode:
             # now, we have to remove successor from the right child
             node.left = _remove(node.left, predecessor.elem)
 
-        return node
+    return node
+
+def _fe_size(node: BinaryNode) -> int:
+    """returns the size balance factor of the input node"""
+    if node is None:
+        return 0
+    return abs(bin._size(node.left) - bin._size(node.right))
+
+def is_size_balanced(tree: BinarySearchTree) -> bool:
+    """return True if the input_tree is size balanced"""
+    return _is_size_balanced(tree.root)
+
+def _is_size_balanced(node: BinaryNode) -> bool:
+    """returns True if the node is size balanced;
+    a node is balanced if its size factor is <=1 and
+    its two children are size balanced"""
+    if node:
+        return _fe_size(node) <= 1 and _is_size_balanced(node.left) and \
+               _is_size_balanced(node.right)
+
+    return True
+
+def _fe_height(node: BinaryNode) -> int:
+    """returns the height balance factor of the input node"""
+    if node is None:
+        return 0
+    return abs(bin._height(node.left) - bin._height(node.right))
+
+def is_height_balanced(tree) -> bool:
+    """return True if the input_tree is height balance, that is,
+    if its root is height balanced"""
+    return _is_height_balanced(tree.root)
+
+def _is_height_balanced(node: BinaryNode) -> bool:
+    """return True if the node is balanced, False e.o.c
+    A node is balanced if its height balance <=1 and
+    its two children are height balanced"""
+    if node:
+        return _fe_height(node) <= 1 and \
+            _is_height_balanced(node.left) and \
+            _is_height_balanced(node.right)
+
+    return True
+
+def array2bst(input_list: list) -> BinarySearchTree:
+    """ gets a sorted list and creates a balanced bst"""
+    result = BinarySearchTree()
+    if input_list and len(input_list) >= 1:
+        _array2bst(input_list, 0, len(input_list)-1, result)
+    return result
+
+def _array2bst(input_list: list, start: int, end: int,
+               tree: BinarySearchTree) -> None:
+    if start <= end:
+        index_mid = (start + end) // 2
+        tree.insert(input_list[index_mid])
+        _array2bst(input_list, start, index_mid-1, tree)
+        _array2bst(input_list, index_mid+1, end, tree)
+
+
+def _array2bst2(input_list: list, tree: BinarySearchTree) -> None:
+    if len(input_list) > 0:
+        index_mid = len(input_list) // 2
+        tree.insert(input_list[index_mid])
+
+        _array2bst2(input_list[:index_mid], tree)
+        _array2bst2(input_list[index_mid+1:], tree)
+
 
 
 class BSTRemove2(BinarySearchTree):
@@ -133,15 +192,16 @@ class BSTRemove2(BinarySearchTree):
         # update the root with the new subtree after remove elem
         self._root = _remove(self._root, elem)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    """
     input_tree = BinarySearchTree()
     values = [25, 20, 36, 10, 22, 30, 40, 5, 12, 28, 38, 48]
     for x in values:
         input_tree.insert(x)
     input_tree.draw()
 
-    """
+    
     print("Smallest element: ", get_smallest_ite(input_tree))
     assert get_smallest_ite(input_tree) == min(values)
 
@@ -175,13 +235,89 @@ if __name__ == "__main__":
     print()
     """
 
-    input_tree2 = BSTRemove2()
-    values = [25, 20, 36, 10, 22, 30, 40, 5, 12, 28, 38, 48]
-    for x in values:
-        input_tree2.insert(x)
-    input_tree2.draw()
-    input_tree2.remove(25)
-    input_tree2.draw()
-
-    input_tree.remove(25)
+    input_tree = BSTRemove2()
+    # input_tree = BinarySearchTree()
+    for x in [5, 10, 15, 20, 23, 22, 24, 3, 7]:
+        input_tree.insert(x)
     input_tree.draw()
+    print("is_size_balanced:", is_size_balanced(input_tree))
+    print("is_height_balanced:", is_height_balanced(input_tree))
+
+    input_tree.remove(24)
+    print("after remove 24, a leaf")
+    input_tree.draw()
+
+    print("is_size_balanced:", is_size_balanced(input_tree))
+    print("is_height_balanced:", is_height_balanced(input_tree))
+
+    input_tree.remove(23)
+    print("after remove 23 (only has a left child)")
+    input_tree.draw()
+
+    print("is_size_balanced:", is_size_balanced(input_tree))
+    print("is_height_balanced:", is_height_balanced(input_tree))
+
+    input_tree.remove(20)
+    print("after remove 20 (only has a right child)")
+    input_tree.draw()
+
+    print("is_size_balanced:", is_size_balanced(input_tree))
+    print("is_height_balanced:", is_height_balanced(input_tree))
+
+    input_tree.remove(22)
+    print("after remove 22 (a leaf)")
+    input_tree.draw()
+
+    print("is_size_balanced:", is_size_balanced(input_tree))
+    print("is_height_balanced:", is_height_balanced(input_tree))
+
+    input_tree.remove(10)
+    print("after remove 10 (two children)")
+    input_tree.draw()
+
+    print("is_size_balanced:", is_size_balanced(input_tree))
+    print("is_height_balanced:", is_height_balanced(input_tree))
+
+    input_tree.remove(5)
+    print("after remove 5 (root with two children)")
+    input_tree.draw()
+
+    print("is_size_balanced:", is_size_balanced(input_tree))
+    print("is_height_balanced:", is_height_balanced(input_tree))
+
+    for x in [11, 10, 30]:
+        input_tree.insert(x)
+    print('after insert [11, 10, 30]')
+    input_tree.draw()
+
+    print("is_size_balanced:", is_size_balanced(input_tree))
+    print("is_height_balanced:", is_height_balanced(input_tree))
+
+    input_tree.remove(3)
+    print("after remove 3 (root only right child)")
+    input_tree.draw()
+
+    print("is_size_balanced:", is_size_balanced(input_tree))
+    print("is_height_balanced:", is_height_balanced(input_tree))
+
+    input_tree.remove(7)
+    print("after remove 7 (root only right child)")
+    input_tree.draw()
+
+    print("is_size_balanced:", is_size_balanced(input_tree))
+    print("is_height_balanced:", is_height_balanced(input_tree))
+
+    input_tree.remove(15)
+    print("after remove 15 root, replace with 11")
+    input_tree.draw()
+    print("is_size_balanced:", is_size_balanced(input_tree))
+    print("is_height_balanced:", is_height_balanced(input_tree))
+
+    data = [50, 20, 60, 15, 55, 70, 10, 18, 75, 8, 93]
+    data.sort()
+    print(data)
+    # data = [5, 10, 15, 20, 25, 30]
+    input_tree = array2bst(data)
+    input_tree.draw()
+    print("is_size_balanced:", is_size_balanced(input_tree))
+    print("is_height_balanced:", is_height_balanced(input_tree))
